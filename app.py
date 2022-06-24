0c7b01df015f645d810380d34b94f4fbcf18534b
jackbustertann <jackbustertann@icloud.com>
2022-06-24 17:24:31 +0100

    creating first chart

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 import json
 import pandas as pd
 from jinja2 import Template
+from datetime import datetime, date
+import plotly.express as px
 
 from google.oauth2 import service_account
 from google.cloud import bigquery
@@ -38,7 +40,7 @@ with open('config.json', 'r') as f:
 st.set_page_config(
 )
 
-st.markdown("# Query 1: Progress Over Time")
+st.markdown("# Chart 1: Progress Over Time")
 
 filter_dicts = config['filters']
 
@@ -49,11 +51,26 @@ for filter_dict in filter_dicts:
     filter_input_type = filter_dict['input_type']
     filter_query = filter_dict.get('query', None)
     filter_default = filter_dict.get('default_value', None)
+    filter_datatype = filter_dict.get('datatype', None)
 
     if filter_query is not None:
 
-        filter_options = [option[filter_name] for option in run_query(filter_query)]
-    
+        if filter_input_type != 'slider':
+
+            filter_options = [option[filter_name] for option in run_query(filter_query)]
+        
+        else:
+
+            query_df = pd.DataFrame(run_query(filter_query))
+
+            min_value, max_value = query_df.loc[0, 'min_value'], query_df.loc[0, 'max_value']
+
+            if filter_datatype == 'datetime':
+
+                min_value, max_value = datetime.strptime(min_value, "%Y-%m-%d"), datetime.strptime(max_value, "%Y-%m-%d")
+
+            filter_options = (min_value, max_value)
+
     else:
 
         filter_options = filter_dict['options']
@@ -87,31 +104,71 @@ for filter_dict in filter_dicts:
             filter_default_index
             )
     
+    elif filter_input_type == 'slider':
+
+        if filter_datatype == 'datetime':
+
+            filter_input = st.sidebar.slider(
+                f"Select a {filter_name_str}",
+                value = filter_options,
+                format="YYYY-MM-DD"
+                )
+    
     filter_inputs[filter_name] = filter_input
 
 query_template = Template(
 
 """
-
-SELECT {{ filter_inputs['date_granularity'] }}, SUM({{ filter_inputs['measure'] }})
-FROM `strava-exploration-v2.strava_prod.fct_daily_metrics`
-WHERE 1 = 1
-{% for key, value in filter_inputs.items() -%}
-    {% if key not in ('date_granularity', 'measure') %}
-        {%- if None in value -%}
-            {%- set value_str = value | select('string') | join(', ') -%}
-    AND ( {{ key }} in ({{ value_str }}) OR {{ key }} IS NULL )
-        {%- else -%}
-            {%- set value_str = value | join(', ') -%}
-    AND {{ key }} in ({{ value_str }})
-        {%- endif -%}
-    {% endif %}
-{% endfor %}
-GROUP BY {{ filter_inputs['date_granularity'] }}
+    SELECT 
+        {{ filter_inputs['date_granularity'] }}, 
+        {{ filter_inputs['dimension'] }}, 
+        SUM({{ filter_inputs['measure'] }}) AS {{ filter_inputs['measure'] }}
+    FROM `strava-exploration-v2.strava_prod.fct_daily_metrics`
+    WHERE 1 = 1
+    {%- for key, value in filter_inputs.items() -%}
+        {%- if key not in ('date_granularity', 'measure', 'dimension', 'date_range') -%}
+            {%- if None in value -%}
+                {%- set value_str = value | select('string') | join('", "') -%}
+        AND ( {{ key }} in ("{{ value_str }}") OR {{ key }} IS NULL )
+            {%- else -%}
+                {%- set value_str = value | join('", "') -%}
+        AND {{ key }} in ("{{ value_str }}")
+            {%- endif -%}
+        {%- elif key == 'date_range' -%}
+            {%- set min_date = value[0].strftime('%Y-%m-%d') -%}
+            {%- set max_date = value[1].strftime('%Y-%m-%d') -%}
+        AND CAST(date_day AS DATE) BETWEEN CAST("{{ min_date }}" AS DATE) AND CAST("{{ max_date }}" AS DATE)
+        {%- endif %}
+    {% endfor %}
+    GROUP BY {{ filter_inputs['date_granularity'] }}, {{ filter_inputs['dimension'] }}
+    ORDER BY {{ filter_inputs['date_granularity'] }}
 
 """
 )
 
-query_str = query_template.render(filter_inputs = filter_inputs)
+query_str = query_template.render(
+    filter_inputs = filter_inputs)
 
-st.code(query_str, language = 'sql')
+query_df = pd.DataFrame(run_query(query_str))
+
+n_rows = len(query_df)
+
+if n_rows > 0:
+
+    query_df.columns = [col.replace('_', ' ') for col in list(query_df.columns)]
+
+    query_columns = list(query_df.columns)
+
+    fig = px.bar(query_df, x = query_columns[0], y = query_columns[2], color = query_columns[1])
+
+    st.plotly_chart(fig, use_container_width=True)
+
+else: 
+
+    st.text('No data to display!')
+
+see_query = st.checkbox('See Query', True)
+
+if see_query:
+
+    st.code(query_str, language = 'sql')
